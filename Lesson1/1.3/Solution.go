787aec5a62513250173957dbc08c75b6a81598a3
Rogov Alexander <al.rogov@bftcom.com>
2024-10-21 23:02:29 +0300

    Lesson 1.3 complete

---
+++ Lesson1/1.3/Solution.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// 2/3 Первая программа
+// Напишите программу, которая выводит "I like Go!"
+func main() {
+	fmt.Println("I like Go!")
+}
---
+++ Lesson1/1.3/Solution1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// 3/3 Первая программа
+// Напишите программу, которая выведет "I like Go!" 3 раза.
+func main() {
+	for i := 0; i < 3; i++ {
+		fmt.Println("I like Go!")
+	}
+}
