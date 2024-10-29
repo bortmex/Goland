ce2ca010e602690a04b12f5d2d8113cadb7b5c57
Rogov Alexander <al.rogov@bftcom.com>
2024-10-30 00:37:47 +0300

    Lesson 1.12(5/16) complete

--- Lesson1/1.12/Solution.go
+++ Lesson1/1.12/Solution.go
@@ -1,25 +1,32 @@
 package main
 
+import "fmt"
+
 // 5/16 Массивы и срезы
 func main() {
 
-	var workArray = [16]uint8{99, 151, 137, 71, 117, 187, 20, 93, 187, 67, 1, 2, 3, 5, 7, 8}
+	workArray := [10]uint8{}
+	//var workArray = [16]uint8{99, 151, 137, 71, 117, 187, 20, 93, 187, 67, 1, 2, 3, 5, 7, 8}
 
-	var local1 = workArray[workArray[10]]
-	var local2 = workArray[workArray[11]]
+	//workArray[workArray[10]], workArray[workArray[11]] = workArray[workArray[11]], workArray[workArray[10]]
+	//workArray[workArray[12]], workArray[workArray[13]] = workArray[workArray[13]], workArray[workArray[12]]
+	//workArray[workArray[14]], workArray[workArray[15]] = workArray[workArray[15]], workArray[workArray[14]]
 
-	workArray[workArray[11]] = local1
-	workArray[workArray[10]] = local2
+	var n uint8
+	var y uint8
 
-	var local11 = workArray[workArray[11]]
-	var local22 = workArray[workArray[12]]
+	for i := 0; i < 10; i++ {
+		fmt.Scan(&n)
+		workArray[i] = n
+	}
 
-	workArray[workArray[12]] = local11
-	workArray[workArray[11]] = local22
+	for i := 0; i < 3; i++ {
+		fmt.Scan(&n)
+		fmt.Scan(&y)
+		workArray[n], workArray[y] = workArray[y], workArray[n]
+	}
 
-	var local111 = workArray[workArray[12]]
-	var local222 = workArray[workArray[13]]
-
-	workArray[workArray[13]] = local111
-	workArray[workArray[12]] = local222
+	for i := 0; i < len(workArray); i++ {
+		fmt.Print(workArray[i], " ")
+	}
 }
