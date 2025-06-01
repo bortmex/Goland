85cd947b7dedf1c7114d1dca1ff44d42b1ca9858
Rogov Alexander <alexanderogov@list.ru>
2025-06-01 19:05:55 +0300

    Lesson 4.1 complete

---
+++ stepik/Lesson4/4.1/Solution.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func work(x int) int {
+	return x
+}
+
+//	4/6 шагов пройдено
+// Задача FIFO
+// Реализуйте функции для очереди FIFO (First In, First Out (ПЕРВЫЙ пришел, ПЕРВЫЙ вышел)) с помощью списков.
+// Должны быть данные функции:
+// Push (добавление элемента)
+// Pop (удаление элемента и его возврат)
+// printQueue (печать очереди в одну строку без пробелов)
+
+func Push(elem interface{}, queue *list.List) {
+	queue.PushBack(elem)
+}
+
+func Pop(queue *list.List) interface{} {
+	front := queue.Front()
+	queue.Remove(front)
+	return front
+}
+
+func printQueue(queue *list.List) {
+	for elem := queue.Front(); elem != nil; elem = elem.Next() {
+		fmt.Print(elem.Value)
+	}
+}
+
+func main() {
+}
---
+++ stepik/Lesson4/4.1/Solution1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+//	5/6 шагов пройдено
+// Задача - перевот списка (reverse)
+// Вам нужно реализовать функцию, которая принимает list и переворачивает порядок его элементов,
+// так чтобы последний элемент стал первым, предпоследний — вторым, и так далее.
+
+// ReverseList - функция для реверса списка
+func ReverseList(l *list.List) *list.List {
+	reversedList := list.New()
+	for elem := l.Front(); elem != nil; elem = elem.Next() {
+		reversedList.PushFront(elem.Value)
+	}
+	return reversedList
+}
+
+func printList(queue *list.List) {
+	for temp := queue.Front(); temp != nil; temp = temp.Next() {
+		fmt.Print(temp.Value)
+	}
+	fmt.Println()
+}
+
+func main() {
+
+	task := list.New()
+
+	for i := 0; i < 10; i++ {
+		task.PushBack(i)
+	}
+	printList(task)
+	// 0123456789
+
+	printList(ReverseList(task))
+	// 9876543210
+}
